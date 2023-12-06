1b3ae49e4f776143f462063f7d73b6aaad162ff5
bsheps <35780702+bsheps@users.noreply.github.com>
2023-12-05 21:58:52 -0600

    Day 2 puzzle 2

--- day2.mjs
+++ day2.mjs
@@ -9,7 +9,7 @@ const bag = {
 const lines = day2Data.split("\n");
 
 function isRevealImpossible(revealedCubes) {
-  console.debug(revealedCubes);
+  // console.debug(revealedCubes);
   for (let i = 0; i < revealedCubes.length; i += 2) {
     const numCubes = parseInt(revealedCubes[i]);
     const color = revealedCubes[i + 1];
@@ -31,10 +31,48 @@ function isGameImpossible(game) {
 let sum = 0;
 for (const [index, val] of lines.entries()) {
   const game = val.split(":");
-  console.debug(game[0])
+  // console.debug(game[0])
   if (!isGameImpossible(game[1].trim())) {
     sum += index + 1;
   }
 }
 
-console.log(sum);
+console.log("puzzle 1: " + sum);
+
+const getEmptyBag = () => {
+  return { red: 0, green: 0, blue: 0 };
+};
+
+const calcPowerOfCubes = (b) => {
+  return b.red * b.green * b.blue;
+};
+
+function updateOptimizedBag(optimizedBag, sample) {
+  for (let i = 0; i < sample.length; i += 2) {
+    const num = parseInt(sample[i]);
+    const color = sample[i + 1];
+    if (num > optimizedBag[color]) {
+      optimizedBag[color] = num;
+    }
+  }
+}
+
+function getOptimizedBag(samples) {
+  const optimizedBag = getEmptyBag();
+  samples.forEach((sample) => {
+    sample = sample.trim().replace(/,/g, "").split(" ");
+    updateOptimizedBag(optimizedBag, sample);
+    // console.debug(optimizedBag)
+  });
+  return optimizedBag;
+}
+
+sum = 0;
+lines.forEach((line) => {
+  const game = line.split(":")[1].trim();
+  const samples = game.split(";");
+  const optimizedBag = getOptimizedBag(samples);
+  // console.debug(optimizedBag);
+  sum += calcPowerOfCubes(optimizedBag);
+});
+console.log("puzzle 2: " + sum)
