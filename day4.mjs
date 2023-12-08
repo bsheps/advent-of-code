44b0cf4767cebf61f7da37503a452ab03355b4eb
bsheps <35780702+bsheps@users.noreply.github.com>
2023-12-07 21:23:27 -0600

    day 4 puzzle 2

--- day4.mjs
+++ day4.mjs
@@ -16,3 +16,34 @@ cards.forEach((card) => {
   }
 });
 console.log("total points: " + total);
+
+const originalCards = day4Data.split("\n");
+const cardCount = [];
+// fill up starting value
+for (let i = 0; i < originalCards.length; i++) {
+    cardCount[i] = 1;
+}
+
+for (let i = 0; i < originalCards.length; i++) {
+    const card = originalCards[i].replace(/[ ]+/g, " "); // remove excess whitespace
+    const game = card.split(": ");
+    const board = game[1].trim().split(" | ");
+    const winningNumbers = board[0].split(" ");
+    const cardNumbers = board[1].split(" ");
+    const matchingNumbers = winningNumbers.filter((n) =>
+        cardNumbers.includes(n)
+    ).length;
+    // console.debug(matchingNumbers)
+    if (matchingNumbers > 0) {
+        const numOfThisCard = cardCount[i];
+        for(let j = i+1; j <= i + matchingNumbers && j < cardCount.length; j++){
+            cardCount[j] = cardCount[j] + numOfThisCard;
+        }
+    }
+    // console.debug(cardCount)
+}
+let totalScratchoff = 0;
+for(let i =0; i < originalCards.length; i++){
+    totalScratchoff += cardCount[i];
+}
+console.log('total scratchcards: ' + totalScratchoff)
