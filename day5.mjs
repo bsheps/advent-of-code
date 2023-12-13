25b330ee64c60cdba0709b73dc95b4cc789ba18d
bsheps <35780702+bsheps@users.noreply.github.com>
2023-12-12 21:24:27 -0600

    day 5 puzzle 2

--- day5.mjs
+++ day5.mjs
@@ -1,6 +1,6 @@
 import { day5Sample1, day5Data } from "./data.mjs";
 
-const almanac = [];
+let almanac = [];
 const keys = [
     "seed",
     "soil",
@@ -18,30 +18,14 @@ let i = 0;
 const seedsSection = lines[i].split(": ");
 const seedNums = seedsSection[1].split(" ");
 
-/**
- * For puzzle part 1
- */
-// seedNums.forEach((seedNum) => {
-//     let data = {
-//         seed: parseInt(seedNum),
-//     };
-//     almanac.push(data);
-// });
+seedNums.forEach((seedNum) => {
+    let data = {
+        seed: parseInt(seedNum),
+    };
+    almanac.push(data);
+});
 
-/**
- * For puzzle part 2
- */
-let counter = 0;
-for (let index = 0; index < seedNums.length; index += 2) {
-    let startVal = seedNums[index];
-    for (let modifier = 0; modifier < seedNums[index + 1]; modifier++) {
-        let data = {
-            seed: parseInt(startVal) + modifier,
-        };
-        almanac.push(data);
-    }
-}
-console.debug(almanac);
+// console.debug(almanac);
 function mapToObject(map) {
     return {
         destination: parseInt(map[0]),
@@ -107,5 +91,144 @@ function getLowestLocationNumber(almanac) {
     });
     return min;
 }
-console.log(almanac);
+// console.log(almanac);
 console.log(getLowestLocationNumber(almanac));
+
+//----------- part 2 -----------------
+//------------------------------------
+class Mapping {
+    dest;
+    src;
+    range;
+
+    constructor(dest, src, range) {
+        this.dest = parseInt(dest);
+        this.src = parseInt(src);
+        this.range = parseInt(range);
+    }
+    get start() {
+        return this.src;
+    }
+
+    get end() {
+        return this.src + this.range;
+    }
+}
+
+class Range {
+    start;
+    end;
+    constructor(start, end) {
+        this.start = parseInt(start);
+        this.end = parseInt(end);
+    }
+
+    get start() {
+        return this.start;
+    }
+    get end() {
+        return this.end;
+    }
+    get isPositiveRange() {
+        return this.end > this.start;
+    }
+}
+
+let seeds = [];
+let mappings = {};
+
+for (let index = 0; index < seedNums.length; index += 2) {
+    let seedNum = seedNums[index];
+    let seedRange = seedNums[index + 1];
+    let data = {
+        seed: parseInt(seedNum),
+        seedRange: parseInt(seedRange),
+    };
+    seeds.push(data);
+}
+i = 3;
+let keyIndex = 1;
+while (i < lines.length) {
+    let mappingKey = keys[keyIndex++];
+    mappings[mappingKey] = [];
+    while (i < lines.length && lines[i] != "") {
+        let data = lines[i].split(" ");
+        let mapping = new Mapping(data[0], data[1], data[2]);
+        mappings[mappingKey].push(mapping);
+        i++;
+    }
+    i += 2;
+}
+
+/**
+ * heavily inspired by J Paulson
+ *
+ * @param {*} ranges: Array<Range>
+ * @param {*} mappings: Array<Mapping>
+ */
+function findIntervals(ranges, mappings) {
+    let intersectedRanges = [];
+    mappings.forEach((map) => {
+        // console.log(map);
+        let nonIntersectedRanges = [];
+        while (ranges?.length > 0) {
+            // console.log(ranges)
+            const range = ranges.pop();
+
+            // this is the left side of our range that does not intersect with the mapping
+            const intervalLeft = new Range(
+                range.start,
+                Math.min(range.end, map.start)
+            );
+
+            // this is the intersection where our range intersects with the mapping
+            const intersection = new Range(
+                Math.max(range.start, map.start),
+                Math.min(map.end, range.end)
+            );
+
+            // this is the right side of our range that does not intersect with the mapping
+            const intervalRight = new Range(
+                Math.max(map.end, range.start),
+                range.end
+            );
+
+            if (intervalLeft.isPositiveRange) {
+                nonIntersectedRanges.push(intervalLeft);
+            }
+            if (intersection.isPositiveRange) {
+                intersectedRanges.push(
+                    new Range(
+                        intersection.start - map.src + map.dest,
+                        intersection.end - map.src + map.dest
+                    )
+                );
+            }
+            if (intervalRight.isPositiveRange) {
+                nonIntersectedRanges.push(intervalRight);
+            }
+        }
+        ranges = ranges.concat(nonIntersectedRanges);
+    });
+    return intersectedRanges.concat(ranges);
+}
+
+let lowest = Infinity;
+for (const { seed, seedRange } of seeds) {
+    /* Build the pair with inclusive start and exclusive end values
+     *   [a,b) = b-a
+     *   [a,b) + [b,c) = [a,c)
+     */
+    let ranges = [new Range(seed, seed + seedRange)];
+    // console.log("---------------------seeds:");
+    // console.dir(ranges);
+    for (let ki = 1; ki < keys.length; ki++) {
+        ranges = findIntervals(ranges, mappings[keys[ki]]);
+        // console.log(keys[ki] + " ranges:");
+        // console.dir(ranges);
+    }
+    ranges.forEach((range) => {
+        lowest = Math.min(lowest, range.start);
+    });
+}
+console.log(lowest);
